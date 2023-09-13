b6824f2c55427244390fd320a625bae3e50e27ed
CaetanoVeloso <caetanoemanuel@yahoo.com>
2023-09-13 10:24:36 -0300

    Arquivos

---
+++ historico.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Projeto 2Tri - WebDev3</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+
+<body>
+    <header>
+        <h2>Desenvolvimento Web</h2>
+        <a href="../../index.php" class="home"> Home </a>
+    </header>
+    <h1>Histórico</h1>
+
+
+<footer>
+        <p>Caetano Veloso e Tales Cordeiro - &copy;2023</p>
+    </footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Projeto 2Tri - WebDev3</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+
+<body>
+    <header>
+        <h2>Desenvolvimento Web</h2>
+        <a href="#" class="home"> Home </a>
+    </header>
+
+    <main>
+        <h1>Simulador de Investimentos</h1>
+        <ul>
+            <li><a href="./entrada.html">Simulação</a></li>
+            <li><a href="./historico.php">historico</a></li>
+        </ul>
+    </main>
+
+    <footer>
+        <p>Caetano Veloso e Tales Cordeiro - &copy;2023</p>
+    </footer>
+
+</body>
+
+</html>
